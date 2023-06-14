28cfff6fd50e78a7a350626f182b6cdddd04fd7c
Mikungu <gemik80@gmail.com>
2023-06-14 14:33:54 +0200

    Initial Commit

---
+++ TalaOyebaNdembo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TalaOyebaNdembo
+//
+//  Created by Mikungu Giresse on 14/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
