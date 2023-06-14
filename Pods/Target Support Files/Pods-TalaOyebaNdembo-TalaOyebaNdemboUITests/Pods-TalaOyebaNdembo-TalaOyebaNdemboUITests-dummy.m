175571a2b545c032c11678088e9f637337c5688b
Mikungu <gemik80@gmail.com>
2023-06-14 19:53:53 +0200

    alamofire swift packages

--- Pods/Target Support Files/Pods-TalaOyebaNdembo-TalaOyebaNdemboUITests/Pods-TalaOyebaNdembo-TalaOyebaNdemboUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TalaOyebaNdembo_TalaOyebaNdemboUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TalaOyebaNdembo_TalaOyebaNdemboUITests
-@end
--- Pods/Target Support Files/Pods-TalaOyebaNdembo/Pods-TalaOyebaNdembo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TalaOyebaNdembo : NSObject
-@end
-@implementation PodsDummy_Pods_TalaOyebaNdembo
-@end
